bb989a9fea6159bbac1a223738b20b5f5a3f0314
ShuaihaoZhao <zsh9519@gmail.com>
2018-11-11 11:29:17 -0600

    Improve movement functionalities

--- Unity_Project/Assets/Scripts/Movement.cs
+++ Unity_Project/Assets/Scripts/Movement.cs
@@ -8,14 +8,16 @@ public class Movement : MonoBehaviour {
     {
         public float forwardVel = 3;
         public float roatationVel = 100f;
-        public float jumpVel = 12;
-        //public float disToGround = 0.1f;
+        public float jumpVel = 5;
+        public float disToGround = 0.1f;
         public bool ground = true;
+        public float velocityY;
     }
 
     public class PhysSettings
     {
-        public float downAccel = 0.78f;
+        public float downAccel = -0.78f;
+        public float gravity = -9f;
     }
 
     public class InputSettings
@@ -33,17 +35,11 @@ public class Movement : MonoBehaviour {
 
     Vector3 velocity = Vector3.zero;
     private Quaternion m_rotation;//Quaterenion used for rotation
-    private Rigidbody m_rigidbody;
+    private CharacterController controller;
+    //private Rigidbody m_rigidbody;
     private Animator m_animator;
     private AnimatorStateInfo animSta;
     private Transform camera_transform;
-
-    /*
-    private const string BLENDTREE = "Blend Tree";
-    private const string ATTACK1 = "attack1";
-    private const string ATTACK2 = "attack2";
-    private const string ATTACK3 = "attack3";
-    private int hitCount = 0;*/
 
 
     public MoveSettings moveSettings = new MoveSettings();
@@ -56,8 +52,10 @@ public class Movement : MonoBehaviour {
         turnInput = 0;
         jumpInput = 0;
         moveSettings.ground = true;
+        moveSettings.velocityY = 0;
         m_rotation = transform.rotation;//initial the rotation
-        m_rigidbody = GetComponent<Rigidbody>();
+        controller = GetComponent<CharacterController>();
+        //m_rigidbody = GetComponent<Rigidbody>();
         m_animator = GetComponent<Animator>();
         camera_transform = Camera.main.transform;
 
@@ -66,30 +64,31 @@ public class Movement : MonoBehaviour {
     private void Update()
     {
         GetInput();
+        transform.eulerAngles = new Vector3(0, camera_transform.eulerAngles.y, 0);
 
-        transform.eulerAngles = new Vector3(0, camera_transform.eulerAngles.y, 0); 
-        /*
-        animSta = m_animator.GetCurrentAnimatorStateInfo(0);
+        
 
-        if (!animSta.IsName(BLENDTREE) && animSta.normalizedTime > 1f)
-        {
-            m_animator.SetInteger("attack_level", 0);
-            hitCount = 0;
-        }
-
-        if (Input.GetMouseButton(0))
-        {
-            Attack();
-        }*/
-        MouseControll();
     }
 
     private void FixedUpdate()
     {
         Run();
-        m_rigidbody.velocity = transform.TransformDirection(velocity);
-    }
+        Jump();
 
+        moveSettings.velocityY += Time.deltaTime * physSettings.gravity;
+
+        velocity.y = moveSettings.velocityY;
+        velocity = transform.TransformDirection(velocity);
+        controller.Move(velocity*Time.deltaTime);
+
+        if (controller.isGrounded)
+        {
+            m_animator.SetBool("k_jump", false);
+            moveSettings.velocityY =0;
+        }
+
+    }
+    
     public Quaternion Rotation_value
     {
         get { return m_rotation; }
@@ -114,65 +113,39 @@ public class Movement : MonoBehaviour {
     {
         if (Mathf.Abs(forwardInput) > inputSettings.inputDelay && transform.gameObject.GetComponent<Attack>().GetHit()==0 && m_animator.GetBool("k_death")!=true)
         {
-            //m_animator.SetBool("run", true);
             velocity.z = moveSettings.forwardVel * forwardInput;
         }
         else if (Mathf.Abs(forwardInput) < inputSettings.inputDelay && transform.gameObject.GetComponent<Attack>().GetHit() == 0 && m_animator.GetBool("k_death") != true)
         {
-            //m_animator.SetBool("run", false);
-            // m_animator.SetBool("fight", true);
             velocity.z = 0;
         }
 
 
         if (Mathf.Abs(turnInput) > inputSettings.inputDelay && transform.gameObject.GetComponent<Attack>().GetHit() == 0 && m_animator.GetBool("k_death") != true)
         {
-            //m_animator.SetBool("run", false);
-            // m_animator.SetBool("fight", true);
             velocity.x = moveSettings.forwardVel * turnInput;
         }
         else if (Mathf.Abs(turnInput) < inputSettings.inputDelay && transform.gameObject.GetComponent<Attack>().GetHit() == 0 && m_animator.GetBool("k_death") != true)
         {
-            //m_animator.SetBool("run", false);
-            // m_animator.SetBool("fight", true);
             velocity.x = 0;
         }
         else
         {
-            //m_animator.SetBool("run", false);
             velocity.x = 0;
             velocity.z = 0;
         }
     }
-    /*
-    void Attack()
+
+    void Jump()
     {
-        if (animSta.IsName(BLENDTREE) && hitCount == 0 && animSta.normalizedTime > 0.5f)
+        if (Mathf.Abs(jumpInput) > inputSettings.inputDelay && transform.gameObject.GetComponent<Attack>().GetHit() == 0 && m_animator.GetBool("k_death") != true)
         {
-            m_animator.SetInteger("attack_level", 1);
-            hitCount = 1;
+            if (controller.isGrounded)
+            {
+                m_animator.SetBool("k_jump", true);
+                moveSettings.velocityY = Mathf.Sqrt(-2*physSettings.gravity*1);
+            }
         }
-        else if (animSta.IsName(ATTACK1) && hitCount == 1 && animSta.normalizedTime > 0.6f)
-        {
-            m_animator.SetInteger("attack_level", 2);
-            hitCount = 2;
-        }
-        else if (animSta.IsName(ATTACK2) && hitCount == 2 && animSta.normalizedTime > 0.8f)
-        {
-            m_animator.SetInteger("attack_level", 3);
-            hitCount = 3;
-        }
-    }*/
-
-    
-    private void MouseControll()
-    {/*
-        float xPosition = Input.mousePosition.x / Screen.width;
-        //float yPosition = Input.mousePosition.y / Screen.height;
-
-        float yAngle = Mathf.Clamp(xPosition * 720, 30, 360);
-        this.gameObject.transform.eulerAngles = new Vector3(0, yAngle, 0);*/
-
     }
 
     private void OnCollisionEnter(Collision collision)

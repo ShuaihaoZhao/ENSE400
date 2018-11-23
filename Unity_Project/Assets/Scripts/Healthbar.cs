717101db6047808f76a973ba452fd6d3e736e8dc
peng0442 <peng0442@gmail.com>
2018-11-23 02:04:10 -0600

    Add health bar

---
+++ Unity_Project/Assets/Scripts/Healthbar.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Healthbar : MonoBehaviour {
+
+    public Scrollbar HealthBar;
+    public float Health = 100;
+    private GameObject target;
+    private int currentHealth;
+
+
+    private void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Kn");
+    }
+
+    private void Update()
+    {
+        Health_UI();
+    }
+    public void Health_UI()
+    {
+        currentHealth=target.GetComponent<PlayerHealth>().GetCurrentHealth();
+        HealthBar.size = currentHealth / 100f;
+        Debug.Log(HealthBar.size);
+    }
+    /*
+    public void Damage(float value)
+    {
+        Health -= value;
+        HealthBar.size = Health / 100f;
+    }*/
+	
+}

bab3a874d3c169bf5aa0e2bb1c91cf8cec9c8161
ShuaihaoZhao <526312824@qq.com>
2019-01-16 12:05:50 -0600

    Add weapen prefabs

---
+++ Unity_Project/Assets/Scripts/Inventory/Equipment.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Equipment", menuName = "Inventory/Equipment")]
+public class Equipment : Item {
+
+    public int damage;
+    public int armor;
+    public Equip_position position;
+
+    public override void UseItem()
+    {
+        base.UseItem();
+        Debug.Log("Equip " + name);
+        EquipmentManager.instance.Equip(this);
+        Remove();
+    }
+}
+
+public enum Equip_position { weapon,head,armor};
+
+
---
+++ Unity_Project/Assets/Scripts/Inventory/EquipmentManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EquipmentManager : MonoBehaviour {
+
+    #region Singleton
+    public static EquipmentManager instance;
+
+    public void Awake()
+    {
+        instance = this;
+    }
+    #endregion
+
+    Equipment[] currentEquipment;
+    private int index;
+    private GameObject[] player_weapons;
+
+    //public GameObject sword_1;
+    //public GameObject sword_2;
+    //public GameObject halbert_1;
+
+    private void Start()
+    {
+        index = System.Enum.GetNames(typeof(Equip_position)).Length;
+        currentEquipment = new Equipment[index];
+        player_weapons = GameObject.FindGameObjectsWithTag("Kn_w");
+        for (int j = 0; j < player_weapons.Length; j++)//check different weapon states
+        {
+            Debug.Log(player_weapons[j].name);
+            player_weapons[j].SetActive(false);
+        }
+    }
+
+    public void Equip(Equipment newEquipment)
+    {
+        int equipemt_index_numer = (int)newEquipment.position;//different position index number
+
+        currentEquipment[equipemt_index_numer] = newEquipment;
+        Update_newEquipment();
+
+    }
+
+    public void Update_newEquipment()
+    {
+        for (int i = 0; i < index; i++)//check all equipment position
+        {
+            if (currentEquipment[i] != null)
+            {
+                string equipmentName = currentEquipment[i].name;
+                for (int j = 0; j < player_weapons.Length; j++)//check different weapon states
+                {
+                    player_weapons[j].SetActive(false);
+                    if (player_weapons[j].name == equipmentName)
+                    {
+                        player_weapons[j].SetActive(true);
+                    }
+                }
+
+            }
+        }
+    }
+}

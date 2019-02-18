7fea77348333f03f555dbcce98cafb01f44a8382
ShuaihaoZhao <526312824@qq.com>
2019-02-18 13:18:55 -0600

    Add large files
    Try to use lfs tool to update

---
+++ Unity_Project/Assets/Scripts/Inventory/Map_item.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map_item : MonoBehaviour {
+
+    [CreateAssetMenu(fileName = "Map_segment", menuName = "Inventory/map_segment")]
+    public class Map_unlock : Item
+    {
+
+        public override void UseItem()
+        {
+            base.UseItem();
+            GameObject part1 = GameObject.FindGameObjectWithTag("part1_wall");
+
+            part1.SetActive(false);
+            Remove();
+        }
+    }
+}

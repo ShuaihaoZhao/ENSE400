63aef11fb2975727c3ca5d04f6ef24982093b4e1
ShuaihaoZhao <526312824@qq.com>
2019-02-27 21:24:02 -0600

    Fix missing parts(no idea why missing)

--- Unity_Project/Assets/Scripts/Healthbar.cs
+++ Unity_Project/Assets/Scripts/Healthbar.cs
@@ -5,17 +5,28 @@ using UnityEngine.UI;
 
 public class Healthbar : MonoBehaviour {
 
-    public Scrollbar HealthBar;
+    //public Scrollbar HealthBar;
     public float Health = 100;
     public Image health_image;
     private GameObject target;
     private int currentHealth;
 
+    /*
+    public RectTransform health_bar;
+    private float minValue;
+    private float maxValue;
+    private float y_value;
+    */
 
 
     private void Start()
     {
         target = GameObject.FindGameObjectWithTag("Kn");
+        /*
+        y_value = health_bar.position.y;
+        minValue = health_bar.position.x - health_bar.rect.width;
+        maxValue = health_bar.position.x;*/
+
     }
 
     private void Update()
@@ -25,17 +36,28 @@ public class Healthbar : MonoBehaviour {
     public void Health_UI()
     {
         currentHealth=target.GetComponent<Player_stats>().GetCurrentHealth();
-        HealthBar.size= currentHealth / 100f;
+        /*
+        float temp = Map(currentHealth, 0, 100, maxValue, minValue);
 
-        if (HealthBar.size >= 0.5)
+        health_bar.position = new Vector3(temp, y_value);*/
+
+        //HealthBar.size= currentHealth / 100f;
+        health_image.fillAmount = currentHealth / 100f;
+
+        if (currentHealth >= 50)
         {
-            health_image.color = new Color32((byte)(int)(510 * (1 - HealthBar.size)), 255, 0, 255);//calculated slop
+            health_image.color = new Color32((byte)(int)(510 * (1 - health_image.fillAmount)), 255, 0, 255);//calculated slop
         }
         else
         {
-            health_image.color = new Color32(255,(byte)(int)(255-510 * (1 - HealthBar.size)), 0, 255);
+            health_image.color = new Color32(255,(byte)(int)(255-510 * (1 - health_image.fillAmount)), 0, 255);
         }
 
+    }
+
+    private float Map(float x, float in_min, float in_max, float out_min, float out_max)
+    {
+        return (x = in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
     }
 
 

e766c274b3c669f8300ca4af5907a0c8e244da32
ShuaihaoZhao <526312824@qq.com>
2019-03-17 21:35:39 -0600

    Delete the Aura light, update falling rock

---
+++ Unity_Project/Assets/Falling_area.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Falling_area : MonoBehaviour
+{
+    public GameObject rock;
+    private float time = 3f;
+
+    private void Update()
+    {
+        
+        if (time <= 0.5f)
+        {
+            Generate_rocks();
+            time = 3;
+            //Debug.Log("Rock");
+        }
+        time -= Time.deltaTime;
+        //Instantiate(rock, new Vector3(Random.Range(210, 240), 60f, Random.Range(1410, 1430)), Quaternion.identity);
+    }
+
+    void Generate_rocks()
+    {
+        for (int i = 0; i < 8; i++)
+        {
+            Debug.Log("Rock");
+            Instantiate(rock, new Vector3(Random.Range(210, 240), 60f, Random.Range(1410, 1430)), Quaternion.identity);
+        }
+    }
+}
---
+++ Unity_Project/Assets/Scripts/Fall_damage.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fall_damage : MonoBehaviour
+{
+    private bool first;
+
+    private void Start()
+    {
+        first = true;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (first == true)
+        {
+            if (collision.gameObject.tag == "Kn")
+            {
+                collision.gameObject.GetComponent<Player_stats>().Get_Damage(10);
+            }
+            first = false;
+        }
+    }
+
+}

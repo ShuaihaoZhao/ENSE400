8d7f2463b1ad82a5e85322cd400c058267b355cc
ShuaihaoZhao <526312824@qq.com>
2019-03-30 13:20:07 -0600

    Update the display or not function(in game menu)

---
+++ Unity_Project/Assets/Scripts/Stats_folder/UI_manager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_manager : MonoBehaviour
+{
+    public Animator top;
+    public Animator down;
+    private bool check;
+
+
+
+
+    private void Start()
+    {
+        UI_start();
+        check = true;
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            if (check)
+            {
+                UI_end();
+                check = false;
+            }
+            else
+            {
+                UI_start();
+                check = true;
+            }
+        }
+    }
+
+    public void UI_start()
+    {
+        top.SetBool("open", true);
+        down.SetBool("open2", true);
+    }
+
+    public void UI_end()
+    {
+        top.SetBool("open", false);
+        down.SetBool("open2", false);
+    }
+
+}
